4124fae16be02b0f6acea6b063275bc086785470
ajroberto77 <andrew.j.roberto@outlook.com>
2026-08-11 18:58:19 -0400

    Add APOLLO factor snapshot consumer

---
+++ src/neptune/integrations/__init__.py
@@ -0,0 +1,6 @@
+"""Bounded integrations with other Iridium products.
+
+Integration modules must not re-introduce ownership of shared data products into
+Neptune.  They translate approved external contracts into Neptune's portfolio and
+optimizer inputs.
+"""
